eec5ceb04da357134929f00befde2c1989098d94
nnamm <t.nnamm@gmail.com>
2023-10-22 13:59:36 +0900

    added test code

--- main_test.go
+++ main_test.go
@@ -1,32 +1,42 @@
 package main
 
 import (
+	"bytes"
+	"os"
 	"testing"
 )
 
-func TestIsValidInput(t *testing.T) {
+func TestMainFunction(t *testing.T) {
 	tests := []struct {
-		input    string
-		expected bool
+		input    []string
+		expected string
 	}{
-		{"123", true},
-		{"77", true},
-		{"6", true},
-		{"000", true},
-		{"999", true},
-		{"a", false},
-		{"abc", false},
-		{"12a", false},
-		{"1234", false},
-		{"", false},
+		{[]string{"gomd", "12"}, ""}, // delete generated dir after each testing
+		{[]string{"gomd", "abc"}, "invalid argument, expected 1-3 digit number\n"},
+		{[]string{"gomd"}, "expected a single argument\n"},
+		{[]string{"gomd", "12", "12"}, "expected a single argument\n"},
 	}
 
 	for _, test := range tests {
-		t.Run(test.input, func(t *testing.T) {
-			result := isInvalidInput(test.input)
-			if result != test.expected {
-				t.Errorf("isInvalidInput(%s) = %v, expected %v", test.input, result, test.expected)
-			}
-		})
+		os.Args = test.input
+
+		// capture stdout
+		oldStdout := os.Stdout
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+
+		main()
+
+		// reset stdout
+		os.Stdout = oldStdout
+		w.Close()
+
+		var buf bytes.Buffer
+		buf.ReadFrom(r)
+		output := buf.String()
+
+		if output != test.expected {
+			t.Errorf("expected %q but got %q", test.expected, output)
+		}
 	}
 }
